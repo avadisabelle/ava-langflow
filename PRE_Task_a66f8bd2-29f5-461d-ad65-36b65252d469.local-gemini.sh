8b4d9604aebdb66ec63d77cb9c28b7f65011450f
JGWill <jgi@jgwill.com>
2025-11-18 05:44:21 -0500

    local launch to evaluate , contribute, trace and things

---
+++ PRE_Task_a66f8bd2-29f5-461d-ad65-36b65252d469.local-gemini.sh
@@ -0,0 +1,6 @@
+_GEMINII_DEFAULT_MODEL_set pro;geminiid "in @a66f8bd2-29f5-461d-ad65-36b65252d469 is what claude-online prepared for the job.  In /src/home/jgi/.claude/CLAUDE.md are embodiments for yourself todo the job, you would bring yourself in here and create a GEMINI.md for observing and seeing this whole work going on.  in /src/palimpsest/mia-agents/agents/ maybe Mia will see some agents that we can have do some work (maybe claude-online needs our help). as we progress, we might have to create a claude.ai skill in /src/palimpsest/skills-mia/ to help mobile agent contributing.
+if you look in @_env.sh you should understand which trace_id to get and
+ in which session using the coaiapy_aetherial
+We are still at the EAST direction (see: /src/IAIP/directions) and making sure the SOUTH is adequate.
+
+" /src/IAIP /src/_sessiondata/ /src/palimpsest/mia-agents/agents/ /src/home/jgi/.claude /src/palimpsest/skills-mia/
